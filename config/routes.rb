07f41f602540aba11a0bae65250e24cc72a2339e
PlanetEfficacy <jspevack@gmail.com>
2016-09-13 08:28:57 -0600

    adds correct route for categories. All tests pass - 4 examples, 0 failures

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   # resources :categories, param: :name, only: [:show]
   # How do we just get the category name to be the path??
-  resources :categories, only: [:show]
+  # resources :categories, only: [:show]
+
+  get ':categories', to: 'categories#show', as: :category
 end
