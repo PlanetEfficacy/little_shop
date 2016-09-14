11f727845d44d048bd358004c5a646aeacdf0414
tgisg <soniagupta504@gmail.com>
2016-09-14 01:50:16 -0600

    wip changed route to category, added remove from cart feature test and model test

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -10,12 +10,21 @@ class Cart
     contents[item_id.to_s] += 1
   end
 
+  def remove_item(item_id)
+    contents[item_id.to_s] -= 1
+    contents.delete_if {|id, quantity| quantity == 0 }
+  end
+
   def items
     contents.keys.map{ |item_id| Item.find(item_id)}
   end
 
   def total_price
+    if contents.empty?
+      "0.00"
+    else
     contents.map{ |id, quantity| Item.find(id).dollars * quantity }.sum
+    end
   end
 
 end
--- spec/features/user_can_remove_item_from_cart_spec.rb
+++ spec/features/user_can_remove_item_from_cart_spec.rb
@@ -8,29 +8,32 @@
 #   And I should not see the item listed in cart
 # expect(flash[:success]).to be_present
 # expect(ai[:style]).to match(/color: red/)
+require 'rails_helper'
+require 'spec_helper'
 
 RSpec.feature "visitor can remove item from cart" do
   scenario "visitor doesn't see the item in the cart after removing" do
 
-    item = Item.create(title: "Paperclip", description: "This is an artisinal paperclip!", price: 5000, image_url: "http://tectonicablog.com/wp-content/uploads/2013/01/023-455x606.jpg")
-    cart = Cart.new(nil)
-    cart.add_item(item.id)
-    ApplicationController.any_instance.stub(:set_cart).returns(cart)
-    #   When I visit "/cart"
-    visit cart_path
-    #   And I click link "Remove"
+    item = Item.create(title: "Paperclip", description: "This is an artisanal paperclip!", price: 5000, image_url: "http://tectonicablog.com/wp-content/uploads/2013/01/023-455x606.jpg")
+
+    visit items_path
+    click_on "Add to Cart"
+    # as a visitor, when I visit "/cart"
+    click_on "View Cart"
+    # And I click link "Remove"
     click_on "Remove"
-    #   Then my current page should be "/cart"
+    # Then my current page should be "/cart"
     expect(current_path).to eq('/cart')
-    #   And I should see a message styled in green
-    expect(flash[:success][:style]).to match(/color: green/)
-    #   And the message should say "Successfully removed SOME_ITEM from your cart."
-    #   And the title "SOME_ITEM" should be a link to that item in case the user wants to add it back
-    expect(flash[:success]).to have_content("Successfully removed #{link_to item_path(item)} from your cart.")
-    #   And I should not see the item listed in cart
+
+    # And I should see a message styled in green
+    # expect(flash[:success][:style]).to match(/color: green/)
+    # And the message should say "Successfully removed SOME_ITEM from your cart."
+    # And the title "SOME_ITEM" should be a link to that item in case the user wants to add it back
+    expect(page).to have_content("Successfully removed Paperclip from your cart.")
+    # And I should not see the item listed in cart
     within(".invoice_item") do
       expect(page).not_to have_content("Paperclip")
-      expect(page).not_to have_content("This is an artisinal paperclip")
+      expect(page).not_to have_content("This is an artisanal paperclip")
       expect(page).not_to have_content("$50.00")
     end
 
