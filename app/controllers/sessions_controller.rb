82bcf3d86e8c3290d727d4bc8ca799c11f1983b7
PlanetEfficacy <jspevack@gmail.com>
2016-09-14 16:24:36 -0600

    add guest user items spec

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,6 @@ class SessionsController < ApplicationController
     @user = User.find_by(username: params[:session][:username])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      # session_evaluator = SessionEvaluator.new(params[:session])
-      # redirect_to cart_path if session_evaluator.redirect_to_cart
-      # redirect_to dashboard_index_path if session_evaluator.redirect_to_dashboard
       redirect_to dashboard_index_path
     else
       redirect_to login_path
