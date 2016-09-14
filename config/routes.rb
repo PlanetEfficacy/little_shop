3fc4c955ebf9f93035699bc44ea996d0f967f83d
SusiIrwin <susi.irwin@gmail.com>
2016-09-13 21:31:02 -0600

    creates login page for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'items#index'
+
+  get '/login', to: 'sessions#new'
+
   resources :items, only: [:index, :show]
   post '/cart', to: 'carts#create'
   get '/cart', to: 'carts#index'
   get ':categories', to: 'categories#show', as: :category
+
 end
