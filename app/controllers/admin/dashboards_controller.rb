bb1933b3176da56f6d7a66cdfa31fe1f2c0a71f9
PlanetEfficacy <jspevack@gmail.com>
2016-09-16 09:51:00 -0600

    add registered user gets a 404 when visiting admin dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,2 +1,13 @@
 class Admin::DashboardsController < ApplicationController
+  before_action :require_admin
+
+  def require_admin
+    if current_default_user?
+      render :file => "public/404.html", status: :not_found
+    elsif !logged_in?
+
+    else
+
+    end
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,13 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def current_default_user?
+    current_user && !current_user.admin?
+  end
 
   def logged_in?
     !!current_user
