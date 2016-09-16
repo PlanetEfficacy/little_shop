432fec578ccc3bdf201ab4956d4d3d9ed21b3351
PlanetEfficacy <jspevack@gmail.com>
2016-09-16 11:16:28 -0600

    fix database migration

--- db/schema.rb
+++ db/schema.rb
@@ -38,13 +38,11 @@ ActiveRecord::Schema.define(version: 20160916034205) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "username"
     t.string   "password_digest"
-    t.string   "password_confirmation"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
-    t.integer  "role",                  default: 0
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "role",            default: 0
   end
 
 end
