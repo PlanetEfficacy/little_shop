acd9ab36d31f1c3c316ea2e478d58cef59fa8d5c
tgisg <soniagupta504@gmail.com>
2016-09-17 16:52:52 -0600

    fix order show and create functionality

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,16 +3,19 @@ class OrdersController < ApplicationController
     @orders = current_user.orders
   end
 
+  def create
+    @cart = session[:cart]
+    @user = current_user
+    order_compiler = OrderCompiler.new(@cart, @user)
+    order_compiler.populated_item_order
+    flash[:success] = "Order was successfully placed."
+    redirect_to orders_path
+  end
+
   def show
     @order = Order.find(params[:id])
     @items = @order.items
   end
 
-  def create
-    @cart = session[:cart]
-    @user = current_user
-    OrderCompiler.new(@cart, @user)
-    flash[:success] = "Order was successfully placed."
-    redirect_to orders_path
-  end
+
 end
