cd56fccced13c703877a8ae4b14231052aa6489e
PlanetEfficacy <jspevack@gmail.com>
2016-09-17 12:36:09 -0600

    implement unauthenticated users cannot view another user's private data

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,10 @@
 class OrdersController < ApplicationController
+  before_action :require_user
+
+  def require_user
+    redirect_to login_path if !logged_in?
+  end
+
   def index
     @orders = current_user.orders
   end
