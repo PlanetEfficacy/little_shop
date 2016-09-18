84bf4025686490462b9391f3e8d8070a81c9ca26
PlanetEfficacy <jspevack@gmail.com>
2016-09-18 17:30:15 -0600

    add user authorization to user dashboard controller closes card#84

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,7 +1,3 @@
 class BaseController < ActionController::Base
-  before_action :require_user
 
-  def require_user
-    # redirect_to login_path if !logged_in?
-  end
 end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,11 @@
 class DashboardsController < ApplicationController
+  before_action :require_user
+
   def show
     @user = User.find(session[:user_id])
+  end
+
+  def require_user
+    redirect_to login_path if !logged_in?
   end
 end
