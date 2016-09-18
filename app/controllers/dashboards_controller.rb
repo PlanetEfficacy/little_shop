efc29e5cc5b21153297ad8833eaa7cddbf1f1828
PlanetEfficacy <jspevack@gmail.com>
2016-09-18 17:41:28 -0600

    refactor order_compiler and orders_controller closes card#92 and card#85

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,7 @@
 class DashboardsController < ApplicationController
   before_action :require_user
-
+  
   def show
     @user = User.find(session[:user_id])
-  end
-
-  def require_user
-    redirect_to login_path if !logged_in?
   end
 end
--- app/models/order_compiler.rb
+++ app/models/order_compiler.rb
@@ -1,3 +1,20 @@
+# class OrderCompiler
+#   attr_reader :cart, :order
+#
+#   def initialize(cart, current_user)
+#     @cart = cart
+#     @order = Order.create(user_id: current_user.id)
+#   end
+#
+#   def compile
+#     invoice = cart.map do |item_id, quantity|
+#       ItemOrder.create(item_id: item_id.to_i, quantity: quantity, order_id: order.id)
+#     end
+#     invoice
+#   end
+# end
+
+
 class OrderCompiler
   attr_reader :cart, :order
 
@@ -6,10 +23,11 @@ class OrderCompiler
     @order = Order.create(user_id: current_user.id)
   end
 
-  def populated_item_order
-    invoice = cart.map do |item_id, quantity|
-      ItemOrder.create(item_id: item_id.to_i, quantity: quantity, order_id: order.id)
+  def create
+    cart.map do |item_id, quantity|
+      ItemOrder.create( item_id: item_id.to_i,
+                        quantity: quantity,
+                        order_id: order.id  )
     end
-    invoice
   end
 end
