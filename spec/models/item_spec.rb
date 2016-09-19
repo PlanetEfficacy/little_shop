cc52c68b159c712b804a28bf8f0e20c7b74e8fb5
PlanetEfficacy <jspevack@gmail.com>
2016-09-18 21:00:26 -0600

    remove comments from item_spec.rb closes card#90

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # it "has many category_items" do
-  #   should have_many(:categories)
-  # end
-  #
-  # it "has many categorizations" do
-  #   should have_many(:category_items)
-  # end
-  #
-  # it "has many item orders" do
-  #   should have_many(:item_orders)
-  # end
-  #
-  # it "has many orders" do
-  #   should have_many(:orders)
-  # end
-
   context "validations" do
     it { should have_many(:orders) }
     it { should have_many(:item_orders) }
