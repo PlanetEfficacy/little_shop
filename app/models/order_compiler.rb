b010551a814c34d22044a870adb4cdf36696f2a8
PlanetEfficacy <jspevack@gmail.com>
2016-09-18 21:04:16 -0600

    remove comments from order_compiler closes card #92

--- app/models/order_compiler.rb
+++ app/models/order_compiler.rb
@@ -1,20 +1,3 @@
-# class OrderCompiler
-#   attr_reader :cart, :order
-#
-#   def initialize(cart, current_user)
-#     @cart = cart
-#     @order = Order.create(user_id: current_user.id)
-#   end
-#
-#   def compile
-#     invoice = cart.map do |item_id, quantity|
-#       ItemOrder.create(item_id: item_id.to_i, quantity: quantity, order_id: order.id)
-#     end
-#     invoice
-#   end
-# end
-
-
 class OrderCompiler
   attr_reader :cart, :order
 
