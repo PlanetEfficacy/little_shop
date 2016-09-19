8bc88c42ab68c1c48a32a4b877388da7927f1822
David Davydov <kctrlv@gmail.com>
2016-09-19 16:09:31 -0600

    add first test admin can create item without verifications

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,31 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def new
+    @item = Item.new
+    @categories = Category.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    # require "pry"; binding.pry
+    # params['item']['category_ids'].each do |id|
+    item_params['category_ids'].reject{ |id| id==''}.each do |id|
+      @item.categories << Category.find(id)
+    end
+    if @item.save
+      flash[:success] = "Item was successfully saved."
+      redirect_to item_path(@item)
+    else
+      flash[:danger] = "Item could not be saved."
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image_url, :category_ids => [] )
+  end
 end
---
+++ spec/features/admin_can_create_items_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.feature "admin can create items" do
+  scenario "admin successfully creates item" do
+    # As an authenticated Admin:
+    admin = Fabricate(:user, role: 1)
+    category_1 = Fabricate(:category)
+    category_2 = Fabricate(:category)
+
+    visit login_path
+    fill_in "Username", with: admin.username
+    fill_in "Password", with: admin.password
+    click_button "Login"
+    # I can create an item.
+    expect(current_path).to eq(admin_dashboard_path)
+    click_link "Create an Item"
+    expect(current_path).to eq(new_item_path)
+    # - An item must have a title, description and price.
+    fill_in "Title", with: "ItemTitle"
+    fill_in "Description", with: "ItemDescription"
+    fill_in "Price", with: 10945
+    # - An item must belong to at least one category.
+    check "#{category_1.name}"
+    check "#{category_2.name}"
+
+    click_button "Create New Item"
+    expect(current_path).to eq(item_path(Item.last))
+    expect(page).to have_content("ItemTitle")
+    expect(page).to have_content("ItemDescription")
+    expect(page).to have_content("$109.45")
+
+    # - The title and description cannot be empty.
+    # - The title must be unique for all items in the system.
+    # - The price must be a valid decimal numeric value and greater than zero.
+    # - The photo is optional. If not present, a stand-in photo is used. (PAPERCLIP)
+  end
+
+
+
+
+  scenario "visitor cannot create item" do
+  end
+
+  scenario "logged-in user cannot create item" do
+  end
+
+
+end
