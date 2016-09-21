9317a4d4a4480dccea1dbcf21d52add1fa08d4ee
PlanetEfficacy <jspevack@gmail.com>
2016-09-21 17:34:08 -0600

    remove comments from user can change quantity of item in cart

--- spec/features/user_can_change_quantity_of_item_in_cart_spec.rb
+++ spec/features/user_can_change_quantity_of_item_in_cart_spec.rb
@@ -6,47 +6,30 @@ RSpec.feature "visitor can change quantity of item in the cart" do
     visit items_path
     click_on "Add to Cart"
     visit cart_path
-
     within(".invoice_item") do
       expect(page).to have_content(item.title)
       expect(page).to have_content("1")
     end
-
-
-
-    # And when I increase the quantity
     click_button "increase"
-    # Then my current page should be '/cart'
-
     expect(current_path).to eq("/cart")
     within(".invoice_item") do
       expect(page).to have_content(item.title)
-
       expect(page).to have_content("2")
     end
-    # And the subtotal for that item should increase
     within(".invoice_item") do
       expect(page).to have_content("$#{2 * item.price}")
-
     end
-
     within(".invoice_total") do
       expect(page).to have_content("$#{2 * item.price}")
     end
-
-    # And when I decrease the quantity
     click_on "decrease"
-    # Then my current page should be '/cart'
-
     expect(current_path).to eq("/cart")
     within(".invoice_item") do
       expect(page).to have_content(item.title)
       expect(page).to have_content("1")
     end
-    # And the subtotal for that item should decrease
     within(".invoice_item") do
       expect(page).to have_content("$#{item.price}")
-
     end
     within(".invoice_total") do
       expect(page).to have_content("$#{item.price}")
