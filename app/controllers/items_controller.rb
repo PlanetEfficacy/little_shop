da79e2965f707124588aced89919771f9be17039
tgisg <soniagupta504@gmail.com>
2016-09-21 14:42:07 -0600

    refactor items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def new
-    if !logged_in?
-      redirect_to login_path
-    elsif current_default_user?
-      render :file => "public/404.html", status: :not_found
-    end
+    check_user_authorization
     @item = Item.new
     @categories = Category.all
   end
@@ -22,9 +18,31 @@ class ItemsController < ApplicationController
   def create
     redirect_to login_path if current_default_user? || !logged_in?
     @item = Item.new(item_params)
+    assign_valid_category_ids(item_params, @item)
+    check_if_item_can_be_saved(@item)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :price, :image, :category_ids => [] )
+  end
+
+  def check_user_authorization
+    if !logged_in?
+      redirect_to login_path
+    elsif current_default_user?
+      render :file => "public/404.html", status: :not_found
+    end
+  end
+
+  def assign_valid_category_ids(item_params, item)
     item_params['category_ids'].reject{ |id| id==''}.each do |id|
       @item.categories << Category.find(id)
     end
+  end
+
+  def check_if_item_can_be_saved(item)
     if @item.save
       flash[:success] = "Item was successfully saved."
       redirect_to item_path(@item)
@@ -34,9 +52,4 @@ class ItemsController < ApplicationController
     end
   end
 
-  private
-
-  def item_params
-    params.require(:item).permit(:title, :description, :price, :image, :category_ids => [] )
-  end
 end
