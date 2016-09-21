f5970222ae8a8c84e92a38f8410255a66c7c491f
David Davydov <kctrlv@gmail.com>
2016-09-21 15:41:18 -0600

    review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :item
+  validates :stars, presence: true
+  validates :body, presence: true
+  validates_length_of :body, minimum: 15, on: :create
 end
