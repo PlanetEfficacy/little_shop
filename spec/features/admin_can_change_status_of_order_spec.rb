61822a527340ab4b1d02ee9800df2b4daea81b05
David Davydov <kctrlv@gmail.com>
2016-09-21 14:41:10 -0600

    feature_admin_can_change_status_cleaned

--- spec/features/admin_can_change_status_of_order_spec.rb
+++ spec/features/admin_can_change_status_of_order_spec.rb
@@ -1,70 +1,37 @@
 require 'rails_helper'
 
-RSpec.feature "admin can change status of order" do
-  scenario "and clicking the cancel link associated with an order changes the order status to cancelled" do
-
+RSpec.feature 'admin can change status of order' do
+  before :each do
     admin = Fabricate(:user, role: 1)
-    order1 = Fabricate(:order, status: 0)
-    order2 = Fabricate(:order, status: 0)
-
+    @order1 = Fabricate(:order, status: 0)
+    @order2 = Fabricate(:order, status: 1)
     login_as_admin(admin)
-    expect(current_path).to eq(admin_dashboard_path)
+  end
 
+  scenario 'clicking cancel changes status to cancelled' do
     expect(Order.cancelled.count).to eq(0)
-    within('.admin_orders tr:nth-child(2)')do
-      click_link "cancel"
-    end
+    within('.admin_orders tr:nth-child(2)') { click_link 'cancel' }
     expect(Order.cancelled.count).to eq(1)
-    expect(Order.cancelled.first).to eq(order1)
-
+    expect(Order.cancelled.first).to eq(@order1)
     expect(current_path).to eq(admin_orders_cancelled_path)
-    expect(page).to have_content("Order no.#{order1.id} is now cancelled")
-    expect(page).to have_content(order1.id)
-    expect(page).to have_no_content(order2.id)
+    expect(page).to have_content("Order no.#{@order1.id} is now cancelled")
   end
 
-  scenario "and clicking the mark as paid link associated with an order changes the order status to paid" do
-
-    admin = Fabricate(:user, role: 1)
-    order1 = Fabricate(:order, status: 0)
-    order2 = Fabricate(:order, status: 0)
-
-    login_as_admin(admin)
-    expect(current_path).to eq(admin_dashboard_path)
-
-    expect(Order.paid.count).to eq(0)
-    within('.admin_orders tr:nth-child(2)')do
-      click_link "mark as paid"
-    end
-    expect(Order.cancelled.count).to eq(0)
+  scenario 'clicking mark as paid changes status to paid' do
     expect(Order.paid.count).to eq(1)
-    expect(Order.paid.first).to eq(order1)
-
+    within('.admin_orders tr:nth-child(2)') { click_link 'mark as paid' }
+    expect(Order.paid.count).to eq(2)
+    expect(Order.paid.first).to eq(@order1)
     expect(current_path).to eq(admin_orders_paid_path)
-    expect(page).to have_content("Order no.#{order1.id} is now paid")
-    expect(page).to have_content(order1.id)
-    expect(page).to have_no_content(order2.id)
+    expect(page).to have_content("Order no.#{@order1.id} is now paid")
   end
 
-  scenario "and clicking the mark as completed link associated with an order changes the order status to completed" do
-
-    admin = Fabricate(:user, role: 1)
-    order1 = Fabricate(:order, status: 1)
-
-    login_as_admin(admin)
-    expect(current_path).to eq(admin_dashboard_path)
-
+  scenario 'clicking completed changes status to competed' do
     expect(Order.completed.count).to eq(0)
-    within('.admin_orders tr:nth-child(2)')do
-      click_link "mark as completed"
-    end
+    within('.admin_orders tr:nth-child(3)') { click_link 'mark as completed' }
     expect(Order.completed.count).to eq(1)
-    expect(Order.completed.first).to eq(order1)
-
+    expect(Order.completed.first).to eq(@order2)
     expect(current_path).to eq(admin_orders_completed_path)
-    expect(page).to have_content("Order no.#{order1.id} is now completed")
-    expect(page).to have_content(order1.id)
+    expect(page).to have_content("Order no.#{@order2.id} is now completed")
   end
-
-
 end
